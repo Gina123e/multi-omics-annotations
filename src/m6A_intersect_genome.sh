b3f5a41b71fee864e9165ffbabd0d2e1bc35f42c
jiafeng Liao <49948437+liaojf24@users.noreply.github.com>
2025-04-06 21:06:49 +0800

    Update m6A_intersect_genome.sh

--- src/m6A_intersect_genome.sh
+++ src/m6A_intersect_genome.sh
@@ -2,5 +2,7 @@
 
 # generate bed file.[chr,start,end,name,score,strand]
 tail -n +2 ../data/example_m6A.csv | awk '{print $1,$2,$2+1,$5,$10,$3}'  OFS='\t'  > ../data/example_m6A.bed
+
+# consider strand
 bedtools intersect -a ../data/example_m6A.bed -b ../genecode/gencode.v38.annotation.gtf -s -wb > ../data/m6A_intersect_genome.gtf
 
